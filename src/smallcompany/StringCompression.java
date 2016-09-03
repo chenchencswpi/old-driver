7c9e12cd4b5b75db1be0bcbb9d5ed6a14338a48e
chenchen <chenchencswpi@gmail.com>
2016-09-02 23:24:26 -0400

    String compression

--- src/smallcompany/StringCompression.java
+++ src/smallcompany/StringCompression.java
@@ -4,7 +4,11 @@ package smallcompany;
  * Question: Given a string, compress it such as: "aaabb" -> "3a2b".
  * Follow up: If the compressed string's length is longer than orignal one's, no need to compress.
  * 
- * Modification: Suppose
+ * Modification: Suppose the strings are given in a file, and each line only contains one string.
+ * You are given 2 functions, hasNext() is going to return a boolean value to tell you if there are more strings in the file;
+ * next() will return the next string.
+ * 
+ * The point of this question is to ask interviewer how to distinguish the following: 113(does it mean 3 11s or 13 1s).
  * @author cchen
  *
  */
